1b3425fc91077b5e544d18815d7f9ae5156bc7f0
Alex <alexzielonko@gmail.com>
2024-04-30 13:51:27 -0500

    Improve code organization

--- applications/space-data-analyzer/src/app.py
+++ applications/space-data-analyzer/src/app.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
 import pika, sys, os, json
 
+from components.recent_objects_channel import RecentObjectsChannel
+
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('event-collaboration-messaging'))
-    channel = connection.channel()
-    channel.queue_declare(queue='recent_objects')
-
-    def callback(ch, method, properties, body):
-        body = json.loads(body)
-        print(" [x] Received %r" % body)
-
-    channel.basic_consume(queue='recent_objects', on_message_callback=callback, auto_ack=True)
-
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-
+    print('🏃🏻‍♂️ Space Data Analyzer is Running..')
+    RecentObjectsChannel()
+        
 if __name__ == '__main__':
     try:
-      print('🏃🏻‍♂️ Space Data Analyzer is Running..')
       main()
     except KeyboardInterrupt:
         print('Interrupted')
