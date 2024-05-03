74261fd1126148a7b4fc4687890ee570671c6387
Alex <alexzielonko@gmail.com>
2024-05-03 15:58:43 -0500

    Simplify API for continuous deployment debugging

--- applications/sat-scan-api/src/app/__init__.py
+++ applications/sat-scan-api/src/app/__init__.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 # Configurations
 # app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@database/development'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@database/development'
 
 # Define the database object which is imported
 # by modules and controllers
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-from app.space_objects.routes import space_object_routes
+# from app.space_objects.routes import space_object_routes
 
-# Register blueprint(s)
-app.register_blueprint(space_object_routes)
+# # Register blueprint(s)
+# app.register_blueprint(space_object_routes)
 
 @app.route('/health-check')
 def health_check():
