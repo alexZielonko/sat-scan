9b121fba9736996c51203b8ba844ead6dd0d864b
Alex <alexzielonko@gmail.com>
2024-05-03 16:53:23 -0500

    Add ecs_task_execution_role

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -132,12 +132,42 @@ resource "aws_lb_listener" "sat_scan_lb_listener" {
   }
 }
 
+# data "aws_iam_role" "ecs_task_execution_role" {
+#   name = "ecsTaskExecutionRole"
+# }
+
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_execution_IAM_role.html
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "sat-scan-ecs-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
 resource "aws_ecs_task_definition" "sat_scan_ecs_task_definition" {
   family                   = "sat-scan-api-family"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 1024
   memory                   = 2048
+
+  execution_role_arn = aws_iam_role.ecsTaskExecutionRole.arn
 
   container_definitions = <<DEFINITION
 [
