571148179e5e8bd603b75e4ee1994a0b345a8309
Alex <alexzielonko@gmail.com>
2024-05-03 15:54:20 -0500

    Update service names and simplify infrastructure configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -84,7 +84,7 @@ resource "aws_security_group" "sat_scan_web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  // Limit EC2 SSH traffic to local IP
+  // Limit SSH traffic to local IP
   ingress {
     description = "Allow SSH from my computer"
     from_port   = "22"
@@ -108,13 +108,13 @@ resource "aws_security_group" "sat_scan_web_sg" {
 }
 
 resource "aws_lb" "default" {
-  name            = "sat_scan_load_balancer"
+  name            = "sat-scan-load-balancer"
   subnets         = aws_subnet.sat_scan_public_subnet.*.id
   security_groups = [aws_security_group.sat_scan_web_sg.id]
 }
 
 resource "aws_lb_target_group" "sat_scan_lb_target_group" {
-  name        = "sat_scan_lb_target_group"
+  name        = "sat-scan-lb-target-group"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.sat_scan_vpc.id
@@ -133,7 +133,7 @@ resource "aws_lb_listener" "sat_scan_lb_listener" {
 }
 
 resource "aws_ecs_task_definition" "sat_scan_ecs_task_definition" {
-  family                   = "hello-world-app"
+  family                   = "sat-scan-api-family"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 1024
@@ -145,7 +145,7 @@ resource "aws_ecs_task_definition" "sat_scan_ecs_task_definition" {
     "image": "registry.gitlab.com/architect-io/artifacts/nodejs-hello-world:latest",
     "cpu": 1024,
     "memory": 2048,
-    "name": "hello-world-app",
+    "name": "sat-scan-api-family",
     "networkMode": "awsvpc",
     "portMappings": [
       {
@@ -178,13 +178,13 @@ resource "aws_security_group" "sat_scan_api_task_sg" {
 }
 
 resource "aws_ecs_cluster" "main" {
-  name = "sat_scan_cluster"
+  name = "sat-scan-cluster"
 }
 
 resource "aws_ecs_service" "ecs_api_service" {
-  name            = "hello-world-service"
+  name            = "sat-scan-api-service"
   cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.ecs_api_service.arn
+  task_definition = aws_ecs_task_definition.sat_scan_ecs_task_definition.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
@@ -194,95 +194,56 @@ resource "aws_ecs_service" "ecs_api_service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.hello_world.id
-    container_name   = "hello-world-app"
+    target_group_arn = aws_lb_target_group.sat_scan_lb_target_group.id
+    container_name   = "sat-scan-api-family"
     container_port   = 3000
   }
 
-  depends_on = [aws_lb_listener.hello_world]
+  depends_on = [aws_lb_listener.sat_scan_lb_listener]
 }
 
-# resource "aws_security_group" "sat_scan_db_sg" {
-#   name        = "sat_scan_db_sg"
-#   description = "Security group for RDS"
+resource "aws_security_group" "sat_scan_db_sg" {
+  name        = "sat_scan_db_sg"
+  description = "Security group for RDS"
 
-#   vpc_id = aws_vpc.sat_scan_vpc.id
+  vpc_id = aws_vpc.sat_scan_vpc.id
 
-#   // Restrict RDS access to private subnet (not accessible via internet), 
-#   // no inbound/outbound rules provided
+  // Restrict RDS access to private subnet (not accessible via internet), 
+  // no inbound/outbound rules provided
 
-#   // Allow traffic between EC2 security group to the DB security group
-#   ingress {
-#     description     = "Allow PostgreSQL traffic from only the web security group"
-#     from_port       = "5432"
-#     to_port         = "5432"
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.sat_scan_web_sg.id]
-#   }
+  // Only allow traffic within sat scan web security group
+  ingress {
+    description     = "Allow PostgreSQL traffic from only the web security group"
+    from_port       = "5432"
+    to_port         = "5432"
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sat_scan_web_sg.id]
+  }
 
-#   tags = {
-#     Name = "sat_scan_db_sg"
-#   }
-# }
+  tags = {
+    Name = "sat_scan_db_sg"
+  }
+}
 
-# resource "aws_db_subnet_group" "sat_scan_db_subnet_group" {
-#   name        = "sat_scan_db_subnet_group"
-#   description = "DB subnet group"
+resource "aws_db_subnet_group" "sat_scan_db_subnet_group" {
+  name        = "sat_scan_db_subnet_group"
+  description = "DB subnet group"
 
-#   subnet_ids = [for subnet in aws_subnet.sat_scan_private_subnet : subnet.id]
-# }
+  subnet_ids = [for subnet in aws_subnet.sat_scan_private_subnet : subnet.id]
+}
 
-# resource "aws_db_instance" "sat_scan_database" {
-#   allocated_storage = var.settings.database.allocated_storage
-#   engine            = var.settings.database.engine
-#   engine_version    = var.settings.database.engine_version
-#   instance_class    = var.settings.database.instance_class
-#   db_name           = var.settings.database.db_name
+resource "aws_db_instance" "sat_scan_database" {
+  allocated_storage = var.settings.database.allocated_storage
+  engine            = var.settings.database.engine
+  engine_version    = var.settings.database.engine_version
+  instance_class    = var.settings.database.instance_class
+  db_name           = var.settings.database.db_name
 
-#   username = var.db_username
-#   password = var.db_password
+  username = var.db_username
+  password = var.db_password
 
-#   db_subnet_group_name   = aws_db_subnet_group.sat_scan_db_subnet_group.id
-#   vpc_security_group_ids = [aws_security_group.sat_scan_db_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.sat_scan_db_subnet_group.id
+  vpc_security_group_ids = [aws_security_group.sat_scan_db_sg.id]
 
-#   skip_final_snapshot = var.settings.database.skip_final_snapshot
-# }
-
-# resource "aws_key_pair" "sat_scan_kp" {
-#   key_name = "sat_scan_kp"
-
-#   // Used as public key for SSH key
-#   // Key created in same directory as main.tf
-#   public_key = file("sat_scan_kp.pub")
-# }
-
-# // Create API EC2 Instance
-# resource "aws_instance" "sat_scan_api" {
-#   count = var.settings.api.count
-
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = var.settings.api.instance_type
-
-#   subnet_id = aws_subnet.sat_scan_public_subnet[count.index].id
-#   key_name  = aws_key_pair.sat_scan_kp.key_name
-
-#   vpc_security_group_ids = [aws_security_group.sat_scan_web_sg.id]
-
-#   tags = {
-#     Name = "sat_scan_api${count.index}"
-#   }
-# }
-
-# // Create an Elastic IP for each API EC2 instance
-# resource "aws_eip" "sat_scan_api_eip" {
-#   count = var.settings.api.count
-
-#   instance = aws_instance.sat_scan_api[count.index].id
-
-#   // Place Elastic IP in the VPC
-#   vpc = true
-
-#   tags = {
-#     Name = "sat_scan_api_eip_${count.index}"
-#   }
-# }
+  skip_final_snapshot = var.settings.database.skip_final_snapshot
+}
