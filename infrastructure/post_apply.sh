f20bcaddbb77b8fd558708b12b8d49aa45b34ed1
Alex <alexzielonko@gmail.com>
2024-05-07 11:49:13 -0500

    Extent post_apply.sh with basic script to simplify production db migrations

--- infrastructure/post_apply.sh
+++ infrastructure/post_apply.sh
@@ -2,10 +2,16 @@
 
 ###########################################################
 # Script Name: post_apply.sh
-# Description: Uploads endpoints and urls of infrastructure
-#  provisioned by Terraform for injection into application 
-#  code during continuous deployment process
+# Description: 
+# - Run after applying terraform changes
+# - Creates route-config.ini file for downstream application 
+#   useage during continuous integration and   deployment pipelines
+# - Runs database migrations
 ###########################################################
+
+# Create and upload route-config.ini file to S3
+
+echo "👉 Creating route-config.ini file"
 
 S3_BUCKET_URI="s3://sat-scan-route-config"
 OUTPUT_FILE_NAME="route-config.ini"
@@ -32,3 +38,82 @@ aws s3 cp $OUTPUT_FILE_NAME $S3_BUCKET_URI
 echo "Deleted "$OUTPUT_FILE_NAME" file"
 
 rm $OUTPUT_FILE_NAME
+
+# Apply database migrations to RDS
+
+echo "👉 Running database migrations"
+
+api_public_dns=`terraform output -json | jq -r ".api_public_dns.value"`
+
+MIGRATION_DIRECTORY=database-migrations
+
+ssh -i "sat_scan_kp.pem" ubuntu@$api_public_dns << EOF
+  rm -rf $MIGRATION_DIRECTORY
+  mkdir $MIGRATION_DIRECTORY
+  exit
+EOF
+
+db_name=`terraform output -json | jq -r ".database_endpoint.value.name"`
+db_user=`terraform output -json | jq -r ".database_endpoint.value.username"`
+db_pass=`terraform output -json | jq -r ".database_endpoint.value.password"`
+
+db_connection_url="postgresql://${db_user}:${db_pass}@${database_endpoint}/${db_name}"
+
+cat <<EOF > alembic.ini
+  [alembic]
+  script_location = databases
+  version_path_separator = os
+  sqlalchemy.url = ${db_connection_url}
+  [loggers]
+  keys = root,sqlalchemy,alembic
+  [handlers]
+  keys = console
+  [formatters]
+  keys = generic
+  [logger_root]
+  level = WARN
+  handlers = console
+  qualname =
+  [logger_sqlalchemy]
+  level = WARN
+  handlers =
+  qualname = sqlalchemy.engine
+  [logger_alembic]
+  level = INFO
+  handlers =
+  qualname = alembic
+  [handler_console]
+  class = StreamHandler
+  args = (sys.stderr,)
+  level = NOTSET
+  formatter = generic
+  [formatter_generic]
+  format = %(levelname)-5.5s [%(name)s] %(message)s
+  datefmt = %H:%M:%S
+EOF
+
+echo "-------------------------------------"
+echo "⏳ Transfering migration files to EC2"
+echo "-------------------------------------"
+
+scp -r -i sat_scan_kp.pem alembic.ini ubuntu@$api_public_dns:~/$MIGRATION_DIRECTORY
+scp -r -i sat_scan_kp.pem ../databases ubuntu@$api_public_dns:~/$MIGRATION_DIRECTORY
+
+rm alembic.ini
+
+echo "-------------------------------------"
+echo "⏳ Provisioning EC2 instance & Applying Migrations"
+echo "-------------------------------------"
+
+ssh -i "sat_scan_kp.pem" ubuntu@$api_public_dns << EOF
+  sudo apt-get update
+  sudo apt install -y python3-pip \
+    python3.8-venv \
+    libpq-dev
+  python3 -m venv .venv && source .venv/bin/activate
+  cd $MIGRATION_DIRECTORY
+  python3 -m venv .venv && source .venv/bin/activate
+  pip3 install alembic psycopg2 
+  alembic upgrade head
+  exit
+EOF
