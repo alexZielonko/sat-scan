7923993bb378e9a248d3cb517fae51daf64473f9
Alex <alexzielonko@gmail.com>
2024-05-08 14:24:52 -0500

    Debug connection string

--- applications/space-data-collector/function/index.py
+++ applications/space-data-collector/function/index.py
@@ -45,8 +45,8 @@ def get_pika_connection():
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
 
-        # url = f"amqps://{config.user}:{config.password}@{config.broker_id}.mq.{config.region}.amazonaws.com:5671"
-        url = 'amqp://b-c34a5bef-c8f3-4052-a5e6-46ca9d0735fd-1.mq.us-east-2.amazonaws.com:5671'
+        url = f"amqps://{config.user}:{config.password}@{config.broker_id}-1.mq.{config.region}.amazonaws.com:5671"
+        # url = 'amqp://b-c34a5bef-c8f3-4052-a5e6-46ca9d0735fd-1.mq.us-east-2.amazonaws.com:5671'
         print(f'conn url: {url}')
 
         parameters = pika.URLParameters(url)
