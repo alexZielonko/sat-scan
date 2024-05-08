6d5e2f06e65ad62626a6a34677e9a6847cb0bfc1
Alex <alexzielonko@gmail.com>
2024-05-08 15:15:27 -0500

    Remove heartbeat

--- applications/space-data-collector/function/index.py
+++ applications/space-data-collector/function/index.py
@@ -47,11 +47,11 @@ def get_pika_connection():
 
         url = f"amqps://{config.user}:{config.password}@{config.broker_id}-1.mq.{config.region}.amazonaws.com:5671"
         # url = 'amqp://b-1481d37f-fdcc-40c6-b4aa-bd81be63eaef-1.mq.us-east-2.amazonaws.com:5671'
+        #        amqps://sat-scan-rabbit-mq-username:xsDX8qQbGte28m9kqqF9F7ZRy_in2cg6YF@b-1481d37f-fdcc-40c6-b4aa-bd81be63eaef.mq.us-east-2.amazonaws.com:5671
         print(f'conn url: {url}')
 
         parameters = pika.URLParameters(url)
         parameters.ssl_options = pika.SSLOptions(context=ssl_context)
-        parameters.heartbeat = 36000
         
         return parameters
     
