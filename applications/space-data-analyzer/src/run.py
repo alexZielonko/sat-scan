eff90b45a47da795a3d0ed8ba55dd055d3d0da48
Alex <alexzielonko@gmail.com>
2024-05-09 12:07:26 -0500

    Move console output

--- applications/space-data-analyzer/src/run.py
+++ applications/space-data-analyzer/src/run.py
@@ -1,5 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
+    print("👉 Space Data Analyzer starting (🚀)")
     app.run(host="0.0.0.0", port=8000, debug=True)
-    print("👉 Space Data Analyzer is up (🚀)")
