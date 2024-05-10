5ebefee74c281eec37533a20aa293b996dbfb01f
Alex Zielonko <alexzielonko@gmail.com>
2024-05-10 12:52:12 -0500

    Add Lambda cloudwatch trigger (#6)
    * Update lambda function name
    * Add cloudwatch trigger for lambda

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -307,7 +307,7 @@ resource "aws_s3_bucket" "sat_scan_data_collector_s3" {
 module "lambda_function_in_vpc" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "data-collector-lambda"
+  function_name = "data_collector_lambda"
   description   = "Sat Scan Data Collection Cron"
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
@@ -325,6 +325,26 @@ module "lambda_function_in_vpc" {
     aws_security_group.sat-scan-mq-broker-sg.id
   ]
   attach_network_policy = true
+}
+
+resource "aws_cloudwatch_event_rule" "data_collector_lambda_trigger" {
+  name                = "data-collector-lambda-trigger"
+  description         = "Fires every 12 hours"
+  schedule_expression = "rate(12 hours)"
+}
+
+resource "aws_cloudwatch_event_target" "trigger_lambda_on_schedule" {
+  rule      = aws_cloudwatch_event_rule.data_collector_lambda_trigger.name
+  target_id = "lambda"
+  arn       = module.lambda_function_in_vpc.lambda_function_arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_split_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda_function_in_vpc.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.data_collector_lambda_trigger.arn
 }
 
 # -----------------------------------------
