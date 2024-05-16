a6117a1735a140a238528b90b5c87da7aec8f27c
Alex Zielonko <alexzielonko@gmail.com>
2024-05-15 20:47:41 -0500

    [Data-Analyzer] Fix healthcheck (#12)
    Run RabbitMQ pika consumer on a separate thread to unblock Data Analyzer's /health-check endpoint

--- applications/space-data-analyzer/src/run.py
+++ applications/space-data-analyzer/src/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import threading
 from flask import Flask
 
 from components.channels.recent_objects_channel import RecentObjectsChannel
@@ -7,23 +8,25 @@ from components.config_parsers.credentials import Credentials
 from components.config_parsers.route_config import RouteConfig
 
 
+def start_recent_objects_channel_thread():
+    sat_scan_api_key = Credentials().sat_scan_api_key
+
+    channel = RecentObjectsChannel(
+        sat_scan_api_key=sat_scan_api_key, route_config=RouteConfig()
+    )
+    consumer_thread = threading.Thread(target=channel.start)
+    consumer_thread.start()
+
+
 if __name__ == "__main__":
     print("👉 Space Data Analyzer starting (🚀)")
+
+    start_recent_objects_channel_thread()
 
     app = Flask(__name__)
 
     @app.route("/health-check")
     def health_check():
         return "Success", 200
-
-    print("🏃🏻‍♂️ Space Data Analyzer is Running..")
-
-    sat_scan_api_key = Credentials().sat_scan_api_key
-
-    channel = RecentObjectsChannel(
-        sat_scan_api_key=sat_scan_api_key, route_config=RouteConfig()
-    )
-
-    channel.start()
 
     app.run(host="0.0.0.0", port=8000, debug=True)
