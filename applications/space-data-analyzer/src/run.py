e7e89bf1c6680628a383fd09bf59868a7b7fb98f
Alex <alexzielonko@gmail.com>
2024-05-15 20:35:40 -0500

    Run pika channel on separate thread

--- applications/space-data-analyzer/src/run.py
+++ applications/space-data-analyzer/src/run.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
+import threading
 from flask import Flask
 
 from components.channels.recent_objects_channel import RecentObjectsChannel
 from components.config_parsers.credentials import Credentials
 from components.config_parsers.route_config import RouteConfig
 
-
-if __name__ == "__main__":
-    print("👉 Space Data Analyzer starting (🚀)")
-
+def start_recent_objects_channel_thread():
     sat_scan_api_key = Credentials().sat_scan_api_key
 
     channel = RecentObjectsChannel(
         sat_scan_api_key=sat_scan_api_key, route_config=RouteConfig()
     )
+    consumer_thread = threading.Thread(target=channel.start)
+    consumer_thread.start()
 
-    channel.start()
+if __name__ == "__main__":
+    print("👉 Space Data Analyzer starting (🚀)")
+
+    start_recent_objects_channel_thread()
 
     app = Flask(__name__)
 
@@ -25,4 +28,4 @@ if __name__ == "__main__":
         return "Success", 200
 
     app.run(host="0.0.0.0", port=8000, debug=True)
-    print("🏃🏻‍♂️ Space Data Analyzer is Running..")
+
