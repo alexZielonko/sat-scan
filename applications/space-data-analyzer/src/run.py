ff11609d96d771406242bc209b4f606486444104
Alex <alexzielonko@gmail.com>
2024-05-15 20:36:18 -0500

    Fix formatting

--- applications/space-data-analyzer/src/run.py
+++ applications/space-data-analyzer/src/run.py
@@ -7,6 +7,7 @@ from components.channels.recent_objects_channel import RecentObjectsChannel
 from components.config_parsers.credentials import Credentials
 from components.config_parsers.route_config import RouteConfig
 
+
 def start_recent_objects_channel_thread():
     sat_scan_api_key = Credentials().sat_scan_api_key
 
@@ -15,6 +16,7 @@ def start_recent_objects_channel_thread():
     )
     consumer_thread = threading.Thread(target=channel.start)
     consumer_thread.start()
+
 
 if __name__ == "__main__":
     print("👉 Space Data Analyzer starting (🚀)")
@@ -28,4 +30,3 @@ if __name__ == "__main__":
         return "Success", 200
 
     app.run(host="0.0.0.0", port=8000, debug=True)
-
