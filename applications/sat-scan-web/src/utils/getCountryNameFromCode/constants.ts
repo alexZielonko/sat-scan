7a45d3062fd207b0de3e92c87bccd3d9f74ea81d
Alex <alexzielonko@gmail.com>
2024-05-23 12:04:15 -0500

    Expand launch site and country code lists

--- applications/sat-scan-web/src/utils/getCountryNameFromCode/constants.ts
+++ applications/sat-scan-web/src/utils/getCountryNameFromCode/constants.ts
@@ -1,3 +1,6 @@
 export const COUNTRY_CODES: Record<string, string> = {
+  PAKI: "Pakistan",
   PRC: "China",
+  TBD: "To Be Determined",
+  US: "United States",
 };
--- applications/sat-scan-web/src/utils/getLaunchSiteFromCode/constants.ts
+++ applications/sat-scan-web/src/utils/getLaunchSiteFromCode/constants.ts
@@ -1,3 +1,6 @@
 export const LAUNCH_SITES: Record<string, string> = {
   TSC: "Taiyuan Satellite Launch Center",
+  WSC: "Wenchang Satellite Launch Center",
+  XSLC: "Xichang Satellite Launch Center",
+  XSC: "Xichang Satellite Launch Center",
 };
